d14f24dcc164a73709de56dc6cbd26cd54634183
Ben Lusamba <benlusamba@gmail.com>
2018-03-13 14:55:44 -0400

    Quant Dev Data, first commit

---
+++ marketbot.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class MarketbotSpider(scrapy.Spider):
+    name = 'marketbot'
+    allowed_domains = ['https://www.bloomberg.com/quote/SPX:IND','https://www.bloomberg.com/quote/CL1:COM','https://www.bloomberg.com/quote/DXY:CUR','https://www.bloomberg.com/quote/EXI:US']
+    start_urls = ['http://https://www.bloomberg.com/quote/SPX:IND/','https://www.bloomberg.com/quote/CL1:COM','https://www.bloomberg.com/quote/DXY:CUR','https://www.bloomberg.com/quote/EXI:US']
+
+    def parse(self, response):
+        pass
+        Name = response.css(".name::text").extract()
+        Price = response.css(".price::text").extract()
+        Time = response.css(".price-datetime::text").extract()
+
+
+        for item in zip(Name,Price,Time):
+            #create a dictionary to store the scraped info
+            scraped_info = {
+                'Name' : item[0],
+                'Price' : item[1],
+                'Time' : item[2],
+            }
+
+            yield scraped_info
